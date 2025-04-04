a513576f23723d5d3e296a897b20a569956ef83f
Matematico-Nea <rinagan@unicauca.edu.co>
2025-04-04 13:01:58 -0500

    Ultimos ca,bios

---
+++ app/src/main/java/com/example/androidstudio/Aguilas.java
@@ -0,0 +1,12 @@
+package com.example.androidstudio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aguilas extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aguilas);
+    }
+}
---
+++ app/src/main/java/com/example/androidstudio/Huila.java
@@ -0,0 +1,12 @@
+package com.example.androidstudio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Huila extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huila);
+    }
+}
